f5a56a4c823c4259dfd7f70a0791bd5d77ea66bf
Govind96Mahra <163532421+Govindmahra@users.noreply.github.com>
2024-07-27 13:57:34 +0530

    Create wisecow.sh

---
+++ wisecow.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/bash
+
+SRVPORT=4499
+RSPFILE=response
+
+rm -f $RSPFILE
+mkfifo $RSPFILE
+
+get_api() {
+    read line
+    echo $line
+}
+
+handleRequest() {
+    # Process the request
+    get_api
+    mod=$(/usr/games/fortune)
+
+cat <<EOF > $RSPFILE
+HTTP/1.1 200
+
+<pre>`/usr/games/cowsay $mod`</pre>
+EOF
+}
+
+prerequisites() {
+    command -v /usr/games/cowsay >/dev/null 2>&1 &&
+    command -v /usr/games/fortune >/dev/null 2>&1 || 
+        { 
+            echo "Install prerequisites."
+            exit 1
+        }
+}
+
+main() {
+    export PATH=$PATH:/usr/games
+    prerequisites
+    echo "Wisdom served on port=$SRVPORT..."
+
+    while [ 1 ]; do
+        cat $RSPFILE | nc -lN $SRVPORT | handleRequest
+        sleep 0.01
+    done
+}
+
+main
